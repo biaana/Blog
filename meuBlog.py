4d7465837d1e85faf361e5be1e95611c47c62db7
biaana <50604268+biaana@users.noreply.github.com>
2019-10-24 08:51:44 -0300

    Update meuBlog.py
    Banco de Dados

--- meuBlog.py
+++ meuBlog.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Site.db'
+db = SQLAlchemy(app)
 
-app.route("/")
-def index():
-    return render_template("index.html")
-if __name__ == "__main__":
+Class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(20), u, nullable=False)
+
+def__repr__(self):
+    return '<User %r>' %self.login 
+
+if (__name__) == ("__main__"):
     app.run(debug=True)

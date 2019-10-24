a73af89521a6e7e63ecfb88a1a189af116e9d6bc
biaana <50604268+biaana@users.noreply.github.com>
2019-10-24 10:10:40 -0300

    Update meuBlog.py
    usando Banco de dados

--- meuBlog.py
+++ meuBlog.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from form import RegistratioForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Site.db'
+app.config['SECRET_KEY'] ='dtg098g70f86hhfg70f45dsfsf6dg56r'
 db = SQLAlchemy(app)
 
-Class Usuario(db.Model):
+Class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    senha = db.Column(db.String(20), u, nullable=False)
+    password = db.Column(db.String(20), u, nullable=False)
 
 def__repr__(self):
-    return '<User %r>' %self.login 
+    return '<User %r>' %self.username
 
-if (__name__) == ("__main__"):
-    app.run(debug=True)
+@app.route('/')
+def index():
+    return()
